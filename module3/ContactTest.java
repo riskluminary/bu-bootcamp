1141363aab20f68d4041a95bcafb1dce276cbd31
Paul Zurek <riskluminary@gmail.com>
2026-08-23 20:51:49 -0700

    Modify ContactTest to use @BeforeEach

--- module3/ContactTest.java
+++ module3/ContactTest.java
@@ -1,13 +1,23 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 
 
 public class ContactTest {
 
+    private Contact c;
+
+    @BeforeEach
+    void setupTests() {
+
+        c = new Contact("Ada Lovelace", "+1 617 555 0101");
+
+    }
+
+
     @Test
     void constructor_setsNameCorrectly() {
 
-        var c = new Contact("Ada Lovelace", "+1 617 555 0101");
 
         assertEquals("Ada Lovelace", c.getName());
 
@@ -17,7 +27,6 @@ public class ContactTest {
     @Test
     void constructor_setsPhoneCorrectly() {
 
-        Contact c = new Contact("Ada Lovelace", "+1 617 555 0101");
 
         assertEquals("+1 617 555 0101", c.getPhone());
 
@@ -26,26 +35,22 @@ public class ContactTest {
     @Test
     void getName_containsExactString_notTransformed() {
 
-        Contact c = new Contact("Grace hopper", "555-0000");
 
-        assertEquals("Grace hopper", c.getName());
+        assertEquals("Ada Lovelace", c.getName());
     }
 
     @Test
     void toString_containsName() {
 
-        Contact c = new Contact("Alan Turing", "555-0001");
 
-        assertTrue(c.toString().contains("Alan Turing"));
+        assertTrue(c.toString().contains("Ada Lovelace"));
 
     }
 
     @Test
     void toString_containsPhone() {
 
-        Contact c = new Contact("Alan Turing", "555-0001");
-
-        assertTrue(c.toString().contains("555-0001"));
+        assertTrue(c.toString().contains("+1 617 555 0101"));
     }
 
 
